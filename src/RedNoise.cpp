de03726d49ff4f4d05e68a84237b2271c9b03c35
dunglevan-design <dunglevan2001@gmail.com>
2021-12-22 11:02:30 +0700

    add obj loader functions :fire:

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -3,9 +3,112 @@
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <CanvasPoint.h>
+#include <Colour.h>
+#include <cmath>
+#include <CanvasTriangle.h>
+#include <glm/glm.hpp>
+#include <TextureMap.h>
+#include <TexturePoint.h>
+#include <ModelTriangle.h>
+#include <map>
+#include <algorithm>
+#include <RayTriangleIntersection.h>
+
+using namespace std;
+
 
 #define WIDTH 320
 #define HEIGHT 240
+
+
+map<string, Colour> LoadObjMaterial() {
+	map<string, Colour> pallete;
+	pallete["Red"] = Colour("Test", 255, 0, 0);
+	ifstream inputStream("cornell-box.mtl", ifstream::binary);
+	string nextline;
+	string colourname;
+	for (size_t i = 0; i < 23; i++)
+	{
+		getline(inputStream, nextline);
+		if (nextline.empty()) {
+			continue;
+		}
+		vector<string> arr = split(nextline, ' ');
+		if (arr[0] == "newmtl") {
+			colourname = arr[1];
+		}
+		if (arr[0] == "Kd") {
+			int red = (round)(stof(arr[1]) * 255);
+			int green = (round)(stof(arr[2]) * 255);
+			int blue = (round)(stof(arr[3]) * 255);
+			Colour colour = Colour(colourname, red, green, blue);
+			pallete[colourname] = colour;
+		}
+	}
+
+	return pallete;
+}
+
+
+vector<ModelTriangle> LoadObjtriangles() {
+	vector<ModelTriangle> triangles;
+	map<string, Colour>pallete = LoadObjMaterial();
+
+	vector<glm::vec3> vs;
+	float scaling = 0.17;
+	ifstream inputStream("cornell-box.obj", std::ifstream::binary);
+	string nextline;
+	getline(inputStream, nextline);
+	getline(inputStream, nextline);
+	//skip comment line
+	//119 = number of lines - first 2 comment lines.
+	Colour c = Colour();
+	for (size_t i = 0; i < 119; i++)
+	{
+
+		// get first object
+		getline(inputStream, nextline);
+		if (nextline.empty()) {
+			continue;
+		}
+		// get first object
+
+		else if (nextline.at(0) == 'o') {
+			//object
+			continue;
+		}
+		else if (nextline.at(0) == 'u') {
+			//color
+			vector<string> colorlinevector = split(nextline, ' ');
+			c = pallete.find(colorlinevector[1])->second;
+
+		}
+		else if (nextline.at(0) == 'v') {
+			//vectors or facets.
+			vector<string> linevector = split(nextline, ' ');
+			vs.push_back(glm::vec3(stof(linevector[1]) * scaling, stof(linevector[2]) * scaling, stof(linevector[3]) * scaling));
+		}
+
+		else if (nextline.at(0) == 'f') {
+			vector<string> facetvalues = split(nextline, ' ');
+			facetvalues[1].pop_back();
+			facetvalues[2].pop_back();
+			facetvalues[3].pop_back();
+			int firstvalue = stoi(facetvalues[1]) - 1;
+			int secondvalue = stoi(facetvalues[2]) - 1;
+			int thirdvalue = stoi(facetvalues[3]) - 1;
+			//Colour color = Colour("red", 255, 1, 1);
+			ModelTriangle triangle = ModelTriangle(vs[firstvalue], vs[secondvalue], vs[thirdvalue], c);
+
+			triangles.push_back(triangle);
+
+		}
+	}
+	return triangles;
+
+}
+
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
@@ -33,6 +136,9 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 }
 
 int main(int argc, char *argv[]) {
+	vector<ModelTriangle> triangles = LoadObjtriangles();
+	map<string, Colour>pallete = LoadObjMaterial();
+	
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 	while (true) {
