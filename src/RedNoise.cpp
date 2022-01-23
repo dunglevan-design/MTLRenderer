51a5e4b649deaf8fc00e9a00ceeeff6604374aef
dunglevan-design <dunglevan2001@gmail.com>
2022-01-23 16:53:43 +0000

    add texture loader :fire:

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -385,6 +385,116 @@ void drawRandomFilledTriangle(DrawingWindow& window)
 	drawFilledTriangle(triangle, colour, window);
 }
 
+uint32_t findTexture(int x, int y, TextureMap img) {
+	return img.pixels[y * 480 + x];
+}
+void drawTextureLine(CanvasPoint from, CanvasPoint to, DrawingWindow& window, TextureMap img) {
+	float xDiff = to.x - from.x;
+	float yDiff = to.y - from.y;
+	if (xDiff == 0 && yDiff == 0) {
+		return;
+	}
+	float numberOfSteps = std::max(abs(xDiff), abs(yDiff));
+	float xStepSize = xDiff / numberOfSteps;
+	float xTextureStepSize = (to.texturePoint.x - from.texturePoint.x) / numberOfSteps;
+	float yStepSize = yDiff / numberOfSteps;
+	float yTextureStepSize = (to.texturePoint.y - from.texturePoint.y) / numberOfSteps;
+
+	for (size_t i = 0; i <= numberOfSteps; i++)
+	{
+		if (i == numberOfSteps) {
+			cout << "line 198";
+		}
+		float x = from.x + xStepSize * i;
+		float xtexture = from.texturePoint.x + xTextureStepSize * i;
+
+
+		float y = from.y + yStepSize * i;
+		float ytexture = from.texturePoint.y + yTextureStepSize * i;
+
+		uint32_t colour = findTexture(round(xtexture), round(ytexture), img);
+
+		window.setPixelColour(round(x), round(y), colour);
+	}
+}
+
+CanvasPoint findvk(CanvasPoint v0, CanvasPoint v1, CanvasPoint v2) {
+	float ratio = (v1.y - v0.y) / (v2.y - v0.y);
+	int vky = v1.y;
+	int vkx = v0.x + ratio * (v2.x - v0.x);
+
+	int Texturevkx = v0.texturePoint.x + ratio * (v2.texturePoint.x - v0.texturePoint.x);
+	int Texturevky = v0.texturePoint.y + ratio * (v2.texturePoint.y - v0.texturePoint.y);
+	return CanvasPoint(vkx, vky, Texturevkx, Texturevky);
+}
+
+void TextureMappingTriangle(DrawingWindow& window) {
+	TextureMap img = TextureMap("texture.ppm");
+
+	CanvasPoint v0 = CanvasPoint(160, 10, 195, 5);
+	CanvasPoint v1 = CanvasPoint(10, 150, 65, 330);
+	CanvasPoint v2 = CanvasPoint(300, 230, 395, 380);
+
+	drawTextureLine(v0, v1, window, img);
+	CanvasPoint vk = findvk(v0, v1, v2);
+	//fill top triangle  (v0 -> v1/vk)
+	float topmidDiffx = v0.x - v1.x;
+	float topextraDiffx = v0.x - vk.x;
+	float yDiff = v1.y - v0.y;
+	int numberOfSteps = max(std::max(abs(topmidDiffx), abs(topextraDiffx)), abs(yDiff));
+
+	float topmidStepSize = topmidDiffx / numberOfSteps;
+	float topextraStepSize = topextraDiffx / numberOfSteps;
+	float yStepSize = yDiff / numberOfSteps;
+
+
+	float texturev0_vk_x_stepsize = (v0.texturePoint.x - vk.texturePoint.x) / numberOfSteps;
+	float texturev0_vk_y_stepsize = (v0.texturePoint.y - vk.texturePoint.y) / numberOfSteps;
+	float texturev0_v1_x_stepsize = (v0.texturePoint.x - v1.texturePoint.x) / numberOfSteps;
+	float texturev0_v1_y_stepsize = (v0.texturePoint.y - v1.texturePoint.y) / numberOfSteps;
+
+
+	for (size_t i = 0; i <= numberOfSteps; i++)
+	{
+		int TexturePointFromX = round(v0.texturePoint.x - texturev0_v1_x_stepsize * i);
+		int TexturePointFromY = round(v0.texturePoint.y - texturev0_v1_y_stepsize * i);
+		CanvasPoint from = CanvasPoint(v0.x - topmidStepSize * i, v0.y + yStepSize * i, TexturePointFromX, TexturePointFromY);
+		CanvasPoint to = CanvasPoint(v0.x - topextraStepSize * i, v0.y + yStepSize * i, (int)round(v0.texturePoint.x - texturev0_vk_x_stepsize * i), (int)round(v0.texturePoint.y - texturev0_vk_y_stepsize * i));
+		if (i == numberOfSteps - 1) {
+			cout << "line 253";
+		}
+		drawTextureLine(from, to, window, img);
+	}
+
+	//fill bottom triangle (v1, vk -> v2)
+	float extrabottomDiffx = vk.x - v2.x;
+	float midbottomDiffx = v1.x - v2.x;
+	float ydiff = v2.y - vk.y;
+
+	float ydifftexturepoint = std::max(abs(v2.texturePoint.y - vk.texturePoint.y), abs(v2.texturePoint.y - v1.texturePoint.y));
+	float xdifftexturepoint = std::max(abs(v2.texturePoint.x - vk.texturePoint.x), abs(v2.texturePoint.x - v1.texturePoint.x));
+	float numberOfsteps = max(max(max(std::max(abs(extrabottomDiffx), abs(midbottomDiffx)), abs(ydiff)), ydifftexturepoint), xdifftexturepoint);
+
+	float extrabottomStepSize = extrabottomDiffx / numberOfsteps;
+	float midbottomStepSize = midbottomDiffx / numberOfsteps;
+	float ystepSize = ydiff / numberOfsteps;
+
+	float texturev1_v2_x_stepsize = (v1.texturePoint.x - v2.texturePoint.x) / numberOfsteps;
+	float texturev1_v2_y_stepsize = (v1.texturePoint.y - v2.texturePoint.y) / numberOfsteps;
+	float texturevk_v2_x_stepsize = (vk.texturePoint.x - v2.texturePoint.x) / numberOfsteps;
+	float texturevk_v2_y_stepsize = (vk.texturePoint.y - v2.texturePoint.y) / numberOfsteps;
+
+
+	for (size_t i = 0; i <= numberOfsteps; i++)
+	{
+		CanvasPoint From = CanvasPoint(v1.x - midbottomStepSize * i, v1.y + ystepSize * i, (int)round(v1.texturePoint.x - texturev1_v2_x_stepsize * i), (int)round(v1.texturePoint.y - texturev1_v2_y_stepsize * i));
+		CanvasPoint To = CanvasPoint(vk.x - extrabottomStepSize * i, vk.y + ystepSize * i, (int)round(vk.texturePoint.x - texturevk_v2_x_stepsize * i), (int)round(vk.texturePoint.y - texturevk_v2_y_stepsize * i));
+		drawTextureLine(From, To, window, img);
+	}
+	//
+
+}
+
 
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -396,14 +506,21 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
 		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
 		else if (event.key.keysym.sym == SDLK_t) {
-			std::cout << "you selected draw triangle" << std::endl;
+			std::cout << "draw triangle" << std::endl;
 			drawRandomFilledTriangle(window);
 		}
 
 		else if (event.key.keysym.sym == SDLK_i) {
-			std::cout << "you selected interpolate 2D" << std::endl;
+			std::cout << "interpolate 2D" << std::endl;
 			interpolate2D(window);
 		}
+
+		else if (event.key.keysym.sym == SDLK_m) {
+			std::cout << "texture mapping" << std::endl;
+			TextureMappingTriangle(window);
+		}
+
+
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
 		window.savePPM("output.ppm");
 		window.saveBMP("output.bmp");
